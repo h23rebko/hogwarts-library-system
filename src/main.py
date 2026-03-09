65264e28624d01bc29b48a5edac34c7fe4c1da27
h23rebko <h23rebko@du.se>
2026-03-09 10:46:50 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
+from library_service import LibraryService
+from book_service import BookService
+from loan_service import LoanService
+from library_repository import LibraryRepository
 
+def main():
+    repository = LibraryRepository()
+    book_service = BookService(repository)
+    loan_service = LoanService(repository)
+    library_service = LibraryService(book_service, loan_service)
+
+    print("Welcome to Hogwarts Library")
+
+if __name__ == "__main__":
+    main()
