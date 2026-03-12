9a12eb9edc3f341a3567721e2975223d3e6838d2
Rebecca Kols <h23rebko@du.se>
2026-03-12 09:26:54 +0100

    minor changes and comments

--- tests/test_book_service.py
+++ tests/test_book_service.py
@@ -6,3 +6,9 @@ from src.library_repository import LibraryRepository
 
 def test_simple():
     assert 1 == 1
+
+""" 
+Kör alla tester:
+py -m pytest
+Kör endast detta test: 
+py -m pytest tests/test_book_service.py """
